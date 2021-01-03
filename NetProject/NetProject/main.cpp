6dbcf744721475e3cc17d7084b4d3f962e0c62a4
CrabJounrnal <59743349+CrabJounrnal@users.noreply.github.com>
2021-01-04 02:36:52 +0800

    Add files via upload

--- NetProject/NetProject/main.cpp
+++ NetProject/NetProject/main.cpp
@@ -14,6 +14,29 @@ union Address {
 };
 
 void *HookedPtr = nullptr;
+/*
+
+HookedPtr - 0x4E = X_Character ??
+
+float matrix[] = HookedPtr;
+arcsin(matrix[0]) = угол поворота по горизонту (только получение)
+установка: нормаль-вектор vector2 v
+matrix[0] = v.x
+matrix[8] = v.x
+matrix[2] = v.y
+matrix[6] = -v.y
+
+X_Character + 0x72 (HookedPtr + 0x24) = coords (vector3)
+X_Character + 0x22E = X_CharacterProperties
+X_CharacterProperties + 0x85 (X_Character + 0x2B3) = health (float)
+
+вызываем метод (_thiscall, но можно использовать как функцию _fastcall (this, мусор, ...) [вроде]) 
+по адресу 0x00729ED0([this = (void)*(X_Character + 0x398)], (int??)*(X_Character + 0x728))
+возвращает указатель на объект X_GlobalPainkillerSettings??
+
+*(int*)X_GlobalPainkillerSettings = painkillers_amount
+
+*/
 
 extern "C" void _cdecl IGiveUCharacter();
 extern "C" void _stdcall SetFromAsm(void *FromAsmPtr); // вынужденый костыль
