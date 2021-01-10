8b5dbef395c068265043c3d611129656bda7ebe4
CrabJounrnal <59743349+CrabJounrnal@users.noreply.github.com>
2021-01-11 06:21:06 +0800

    Add files via upload

--- NetProject/NetProject/hooks.asm
+++ NetProject/NetProject/hooks.asm
@@ -1,21 +1,36 @@
 .model flat
 
-EXTERN ?FromAsm@@YGXPAX@Z:near
-
-.data
-FunP DD 0
+EXTERN ?FromAsm@@YIXPAX@Z:near
+EXTERN ?HookListC@@YIXPAUnode@@@Z:near
 
 .code
 
 _IGiveUCharacter PROC
 
 	pushad
-	push esi
-	call ?FromAsm@@YGXPAX@Z
+	mov ecx, esi
+	call ?FromAsm@@YIXPAX@Z
 	popad
 	mov eax, 0642ED0h
 	jmp eax
 
 _IGiveUCharacter ENDP
+
+
+_HookList PROC
+
+	
+	pushad		;edi, ebx
+
+	mov ecx, edi
+	mov edx, ebx
+	call ?HookListC@@YIXPAUnode@@@Z
+
+	popad
+	mov eax, dword ptr [edi+4]		;from hooked place
+	test eax, eax
+	ret
+
+_HookList ENDP
 
 END
